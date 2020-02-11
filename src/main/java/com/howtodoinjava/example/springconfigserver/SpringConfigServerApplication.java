91011f0a9bbcac4393882ddad2110d5e4674295d
yswamy <ykovils@gap.com>
2020-02-11 11:19:56 +0530

    initial commit

---
+++ src/main/java/com/howtodoinjava/example/springconfigserver/SpringConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.howtodoinjava.example.springconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Profile;
+
+@EnableConfigServer
+@SpringBootApplication
+//@Profile("development")
+public class SpringConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfigServerApplication.class, args);
+	}
+}
