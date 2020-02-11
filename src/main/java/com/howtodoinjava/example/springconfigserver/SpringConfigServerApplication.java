0fa8439872e1f06130a41de8aad3cfdf201f7dfa
Sidda Raju <rajusidda82@gmail.com>
2020-02-11 11:24:53 +0530

    Update SpringConfigServerApplication.java

--- src/main/java/com/howtodoinjava/example/springconfigserver/SpringConfigServerApplication.java
+++ src/main/java/com/howtodoinjava/example/springconfigserver/SpringConfigServerApplication.java
@@ -12,6 +12,7 @@ public class SpringConfigServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringConfigServerApplication.class, args);
+		//commited by raju
 		
 	}
 }
