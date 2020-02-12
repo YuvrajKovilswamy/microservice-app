e1fbed17090a0be493d60ee08c4ea86d5609700e
Yuvraj Kovil <60209840+YuvrajKovilswamy@users.noreply.github.com>
2020-02-12 12:02:49 +0530

    Update SpringConfigServerApplication.java

--- src/main/java/com/howtodoinjava/example/springconfigserver/SpringConfigServerApplication.java
+++ src/main/java/com/howtodoinjava/example/springconfigserver/SpringConfigServerApplication.java
@@ -12,6 +12,8 @@ public class SpringConfigServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringConfigServerApplication.class, args);
+		
+		System.out.println("lets make some changes");
 	}
 
 }
